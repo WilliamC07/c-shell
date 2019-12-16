52b5aaf87985dfe92324f2ff8f2e45c4c085d2b2
William Cao <williamcao999@gmail.com>
2019-12-15 20:39:22 -0500

    finished assignment

--- main.c
+++ main.c
@@ -6,36 +6,7 @@
 #include <stdlib.h>
 #include <limits.h>
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmissing-noreturn"
-void test(){
-//    char ls_command[] = "echo 1; echo 2 ; echo 3;echo 4;echo 5;";
-//    int arguments_num = num_args(ls_command);
-//    printf("Tested %s\n Arguments: %d Expected %d\n", ls_command, arguments_num, 3);
-//    printf("Running command %s\n", ls_command);
-//    char ** arguments = tokenize(ls_command);
-//    execvp(arguments[0], arguments);
-
-//    char input[] = "echo 1;echo 2 ; echo -e \"; echo Apple\" ;echo -e \"\\x1B[34m ;;Apples \"";
-//    char input[] = "echo -e \"\\x1B[34m ;;Apples \"";
-//    char ** commands = get_commands(input);
-//    for(int i = 0; i < 4; i++){
-//        printf("Command: .%s.\n", commands[i]);
-//        char ** tokens = tokenize_command(commands[i]);
-//        int t_i = 0;
-//        while(tokens[t_i] != NULL){
-//            printf("token: %s\n", tokens[t_i]);
-//            t_i++;
-//        }
-//        free(tokens);
-//    }
-//    free(commands);
-}
-
 int main() {
-//    test();
-//    return 0;
-
     while (1) {
         char input[500] = {'\0'};
         char cwd[PATH_MAX];
@@ -54,6 +25,4 @@ int main() {
 
         run_commands(commands);
     }
-    return 0;
 }
-#pragma clang diagnostic pop
--- parse_input.h
+++ parse_input.h
@@ -5,12 +5,12 @@ void sterialize_input(char *input);
 
 char **get_commands(char *input);
 
-char ** tokenize_command(char *command);
+char **tokenize_command(char *command);
 
-bool redirection_parameters_given(char ** tokens);
+bool redirection_parameters_given(char **tokens);
 
-char * get_standard_output(char **tokens);
+char *get_standard_output(char **tokens);
 
-char * get_standard_input(char **tokens);
+char *get_standard_input(char **tokens);
 
 int find_pipe(char **tokens);
